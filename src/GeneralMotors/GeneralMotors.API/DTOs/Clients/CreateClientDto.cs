c7f9dd3aa50dc07a9d4c01ae258ef57083178531
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:49 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/DTOs/Clients/CreateClientDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GeneralMotors.API.DTOs.Clients;
+
+public class CreateClientDto
+{
+    public string FullName { get; set; }
+    public string Contact { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
+
