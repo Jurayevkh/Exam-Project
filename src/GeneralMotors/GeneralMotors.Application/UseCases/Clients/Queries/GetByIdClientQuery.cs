3311a14eacc0eead5349f8eb2002f2ca07286187
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:46 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Clients/Queries/GetByIdClientQuery.cs
@@ -0,0 +1,9 @@
+﻿using GeneralMotors.Domain.Entities.Clients;
+
+namespace GeneralMotors.Application.UseCases.Clients.Queries;
+
+public class GetByIdClientQuery:IRequest<Client>
+{
+    public int Id { get; set; }
+}
+
