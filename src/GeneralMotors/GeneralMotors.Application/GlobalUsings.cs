2eb4ceca1f514c3a3139d5ffa253fb5222f7664a
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:52:17 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using MediatR;
+global using GeneralMotors.Application.Abstractions;
+global using GeneralMotors.Application.UseCases.Cars.Commands;
+global using GeneralMotors.Domain.Entities.Cars;
+global using System.Reflection;
+global using Microsoft.Extensions.DependencyInjection;
+global using GeneralMotors.Application.UseCases.Dillers.Commands;
+global using GeneralMotors.Domain.Entities.Dillers;
+global using Microsoft.EntityFrameworkCore;
+global using GeneralMotors.Application.UseCases.Dillers.Queries;
+global using GeneralMotors.Domain.Entities.CarTypes;
+global using GeneralMotors.Application.UseCases.CarTypes.Queries;
+
