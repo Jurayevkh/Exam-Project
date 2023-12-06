be2c8998ef57d50e73bc0db5be3aa7fc544e4f25
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:49 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Dillers/Commands/CreateDillerCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace GeneralMotors.Application.UseCases.Dillers.Commands;
+
+public class CreateDillerCommand:IRequest<bool>
+{
+    public string Region { get; set; }
+    public string Contact { get; set; }
+}
+
