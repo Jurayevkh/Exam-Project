12d9472d8b28d958e2ff0b2137dc5772709625a3
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:12 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Dillers/Handlers/GetByIdDillerQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace GeneralMotors.Application.UseCases.Dillers.Handlers;
+
+public class GetByIdDillerQueryHandler : IRequestHandler<GetByIdDillerQuery, Diller>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public GetByIdDillerQueryHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<Diller> Handle(GetByIdDillerQuery request, CancellationToken cancellationToken)
+    {
+        var diller = await _applicationDbContext.Dillers.FirstOrDefaultAsync(dil => dil.Id == request.Id);
+
+        return diller;
+    }
+}
+
