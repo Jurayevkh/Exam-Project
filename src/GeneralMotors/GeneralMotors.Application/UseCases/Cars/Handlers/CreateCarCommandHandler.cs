d3e95bd7b84b246f7034adea6573b256e666da51
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:52:40 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Cars/Handlers/CreateCarCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace GeneralMotors.Application.UseCases.Cars.Handlers;
+
+public class CreateCarCommandHandler : AsyncRequestHandler<CreateCarCommand>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public CreateCarCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    protected override async Task Handle(CreateCarCommand request, CancellationToken cancellationToken)
+    {
+        Car car = new Car()
+        {
+            Name = request.Name,
+            Model = request.Model,
+            Price = request.Price,
+            Color = request.Color,
+            Fuel_Type = request.Fuel_Type,
+            Features = request.Features,
+            Description = request.Description,
+            CreatedAt=request.CreatedAt,
+            CarTypeId = request.CarTypeId,
+            CarImage = request.CarImage,
+        };
+
+        await _applicationDbContext.Cars.AddAsync(car);
+        await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        
+    }
+}
