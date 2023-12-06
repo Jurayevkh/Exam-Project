ee3f36cf6cf8e6e6e1cf2d1b897ef9b94f0b39a6
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:24 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/Controllers/CarClientController.cs
@@ -0,0 +1,33 @@
+﻿using GeneralMotors.API.DTOs.CarClient;
+using GeneralMotors.Application.UseCases.CarClients.Commands;
+using GeneralMotors.Domain.Entities.Cars;
+
+namespace GeneralMotors.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarClientController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CarClientController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateCarClient(CreateCarClientDto createCarClientDto)
+        {
+            CreateCarClientCommand client = new CreateCarClientCommand()
+            {
+                CarId=createCarClientDto.CarId,
+                UserName=createCarClientDto.UserName
+            };
+
+            var result=await _mediator.Send(client);
+
+            return Ok(result);
+        }
+    }
+}
+
