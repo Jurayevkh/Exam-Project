d55b54796901116a5f086580f2c79083526384b9
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:52:25 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/CarClients/Commands/CreateCarClientCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace GeneralMotors.Application.UseCases.CarClients.Commands;
+
+public class CreateCarClientCommand:IRequest<bool>
+{
+    public int CarId { get; set; }
+    public string UserName { get; set; }
+}
+
