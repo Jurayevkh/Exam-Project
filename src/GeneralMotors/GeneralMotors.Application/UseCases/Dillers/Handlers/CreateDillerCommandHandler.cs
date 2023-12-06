27af456a764cb87b22ca46e71c3022cea6e9b99d
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:59 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Dillers/Handlers/CreateDillerCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace GeneralMotors.Application.UseCases.Dillers.Handlers;
+
+public class CreateDillerCommandHandler : IRequestHandler<CreateDillerCommand,bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public CreateDillerCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(CreateDillerCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Diller diller = new Diller()
+            {
+                Region = request.Region,
+                Contact = request.Contact
+            };
+
+            await _applicationDbContext.Dillers.AddAsync(diller);
+            var result=await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+          return false;
+        }
+
+
+    }
+}
+
