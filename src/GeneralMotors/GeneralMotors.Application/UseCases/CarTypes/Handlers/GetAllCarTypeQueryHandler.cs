d3d4c5f1429f5a7c5f7d391126973da61a8ce992
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:01 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/CarTypes/Handlers/GetAllCarTypeQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace GeneralMotors.Application.UseCases.CarTypes.Handlers;
+
+public class GetAllCarTypeQueryHandler : IRequestHandler<GetAllCarTypesQuery, List<CarType>>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public GetAllCarTypeQueryHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<List<CarType>> Handle(GetAllCarTypesQuery request, CancellationToken cancellationToken)
+    {
+        return await _applicationDbContext.CarTypes.ToListAsync(cancellationToken);
+    }
+}
+
