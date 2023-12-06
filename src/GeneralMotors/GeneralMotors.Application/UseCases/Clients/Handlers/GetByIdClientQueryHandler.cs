b064032b08e81a3b507ed6a88efafcb0e037b211
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:36 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Clients/Handlers/GetByIdClientQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using GeneralMotors.Application.UseCases.Clients.Queries;
+using GeneralMotors.Domain.Entities.Clients;
+
+namespace GeneralMotors.Application.UseCases.Clients.Handlers;
+
+public class GetByIdClientQueryHandler : IRequestHandler<GetByIdClientQuery, Client>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public GetByIdClientQueryHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<Client> Handle(GetByIdClientQuery request, CancellationToken cancellationToken)
+    {
+        var client = await _applicationDbContext.Clients.FirstOrDefaultAsync(cl => cl.Id == request.Id);
+
+        return client;
+    }
+}
+
