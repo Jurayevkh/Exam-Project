217ce0fc5eddb12d5e9769ca15fb514862d96b45
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:55:10 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using GeneralMotors.Application.Abstractions;
+using GeneralMotors.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GeneralMotors.Infrastructure;
+
+public static class DependencyInjection
+{ 
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext,GeneralMotorDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
+
+        return services;
+    }
+}
+
