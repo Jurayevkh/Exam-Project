d1fa18c604c23ef9898eb31dd313d974a66f77d3
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:31 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/Controllers/CarTypeController.cs
@@ -0,0 +1,34 @@
+﻿using GeneralMotors.Application.UseCases.CarTypes.Queries;
+
+namespace GeneralMotors.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CarTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllCarType()
+        {
+            var carType = await _mediator.Send(new GetAllCarTypesQuery());
+
+            return Ok(carType);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdCarType(int id)
+        {
+            var carType = await _mediator.Send(new GetByIdCarTypeQuery() { Id = id });
+
+            return Ok(carType);
+        }
+
+    }
+}
+
