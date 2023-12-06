6b2086798a8af4de718e83727c324cc6d3182941
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:55:18 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Infrastructure/Configurations/SuperAdminTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using GeneralMotors.Domain.Entities.SuperAdmins;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeneralMotors.Infrastructure.Configurations;
+
+public class SuperAdminTypeConfiguration : IEntityTypeConfiguration<SuperAdmin>
+{
+    public void Configure(EntityTypeBuilder<SuperAdmin> builder)
+    {
+        builder.HasData(
+            new SuperAdmin { Id=1,Name="Rustambek Jo'rayev",UserName="rustam",Password="qwerty123"},
+            new SuperAdmin { Id=2,Name="Bahriddin Abdusalomov",UserName="baxa_tashkentskiy",Password="BaxaKriminal"}
+            );
+    }
+}
+
