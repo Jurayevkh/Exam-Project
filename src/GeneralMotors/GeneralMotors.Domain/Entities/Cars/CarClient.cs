c08e8de961149c69e6cdce7954bf19bbe2451a80
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:39 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Domain/Entities/Cars/CarClient.cs
@@ -0,0 +1,13 @@
+﻿using GeneralMotors.Domain.Entities.Clients;
+
+namespace GeneralMotors.Domain.Entities.Cars;
+
+public class CarClient:BaseEntity
+{
+    public int CarId { get; set; }
+    public Car Car { get; set; }
+
+    public int ClientId { get; set; }
+    public Client Client { get; set; }
+}
+
