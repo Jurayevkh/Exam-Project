ddc71e9d09ce4759f79bdffcea091ab2f7ff75e8
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:21 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Clients/Handlers/CreateClientCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using GeneralMotors.Application.UseCases.Clients.Commands;
+using GeneralMotors.Domain.Entities.Clients;
+
+namespace GeneralMotors.Application.UseCases.Clients.Handlers;
+
+public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public CreateClientCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+    public async Task<bool> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Client client = new Client()
+            {
+                FullName=request.FullName,
+                Contact=request.Contact,
+                UserName=request.UserName,
+                Password=request.Password,
+                Email=request.Email,
+                Address=request.Address
+            };
+
+            await _applicationDbContext.Clients.AddAsync(client);
+            var result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
