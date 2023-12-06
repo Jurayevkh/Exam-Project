c0a2b3f85dad0327796e97d3b2abf7d62ca20fe7
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:13 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Clients/Commands/CreateClientCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace GeneralMotors.Application.UseCases.Clients.Commands;
+
+public class CreateClientCommand:IRequest<bool>
+{
+    public string FullName { get; set; }
+    public string Contact { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Role { get; set; } = "Client";
+}
+
