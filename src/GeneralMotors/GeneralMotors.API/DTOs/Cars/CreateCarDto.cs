9eadd2f07b10c3cf64a671cda2dbf335d9d0b10b
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:42 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/DTOs/Cars/CreateCarDto.cs
@@ -0,0 +1,17 @@
+﻿namespace GeneralMotors.API.DTOs.Cars;
+
+public class CreateCarDto
+{
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public double Price { get; set; }
+    public string Color { get; set; }
+    public string Fuel_Type { get; set; }
+    public string Features { get; set; }
+    public string Description { get; set; }
+    public int CarTypeId { get; set; }
+    public string CarImage { get; set; }
+}
+
+
+
