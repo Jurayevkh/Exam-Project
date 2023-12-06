3ceb4cd03c7186e4407f36b32c796c1b95a3933f
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:56 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Dillers/Commands/UpdateDillerCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace GeneralMotors.Application.UseCases.Dillers.Commands;
+
+public class UpdateDillerCommand:IRequest<bool>
+{
+    public int Id { get; set; }
+    public string Region { get; set; }
+    public string Contact { get; set; }
+}
+
