db0096b28e14b90475f247f788ad0a0b19fcd294
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:36 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Domain/Entities/Cars/Car.cs
@@ -0,0 +1,25 @@
+﻿using GeneralMotors.Domain.Entities.CarTypes;
+
+namespace GeneralMotors.Domain.Entities.Cars;
+
+public class Car:BaseEntity
+{
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public double Price { get; set; }
+    public string Color { get; set; }
+    public string Fuel_Type { get; set; }
+    public string Features { get; set; }
+    public string Description { get; set; }
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+    public string CarImage { get; set; }
+
+#region relation
+    public int CarTypeId { get; set; }
+    public CarType CarType { get; set; }
+
+    public ICollection<CarClient> CarClients { get; set; }
+#endregion
+}
+
