b9778fbe61d7c57ba89983db3a9117121ac17bfe
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:52:22 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GeneralMotors.Domain.Entities.Cars;
+using GeneralMotors.Domain.Entities.CarTypes;
+using GeneralMotors.Domain.Entities.Clients;
+using GeneralMotors.Domain.Entities.Dillers;
+using GeneralMotors.Domain.Entities.SuperAdmins;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeneralMotors.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Diller> Dillers { get; set; }
+    public DbSet<CarClient> CarClients { get; set; }
+    public DbSet<SuperAdmin> SuperAdmins { get; set; }
+    public DbSet<CarType> CarTypes { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken=default);
+
+}
+
