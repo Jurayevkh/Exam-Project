406f5a8dff36d153ca03188d960f64476122e3ce
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:17 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using GeneralMotors.API.DTOs.Cars;
+global using GeneralMotors.Application.UseCases.Cars.Commands;
+global using GeneralMotors.Application.UseCases.Cars.Queries;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+
