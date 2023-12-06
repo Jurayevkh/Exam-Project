4c8c2b0fff743bdaabfddf01fd4df613b573bee5
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:53:25 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Clients/Handlers/DeleteClientCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using GeneralMotors.Application.UseCases.Clients.Commands;
+using MediatR;
+
+namespace GeneralMotors.Application.UseCases.Clients.Handlers;
+
+public class DeleteClientCommandHandler : IRequestHandler<DeleteClientCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public DeleteClientCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(DeleteClientCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var client = _applicationDbContext.Clients.FirstOrDefault(cl=>cl.Id==request.Id);
+
+            if (client is null)
+                return false;
+
+            _applicationDbContext.Clients.Remove(client);
+            var result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+
+}
+
