f3b02214bd4134044c0d94e332e3b332b05ad3cd
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:17 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Dillers/Handlers/UpdateDillerCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace GeneralMotors.Application.UseCases.Dillers.Handlers;
+
+public class UpdateDillerCommandHandler:IRequestHandler<UpdateDillerCommand,bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public UpdateDillerCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(UpdateDillerCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var diller = _applicationDbContext.Dillers.FirstOrDefault(dil=>dil.Id==request.Id);
+            if(diller is null)
+                return false;
+            diller.Region = request.Region;
+            diller.Contact = request.Contact;
+            _applicationDbContext.Dillers.Update(diller);
+            var result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
