a31d57bc33ae3ad9ce3bf01c4558851e88b059a1
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:55:15 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Infrastructure/Configurations/CartypeTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GeneralMotors.Domain.Entities.CarTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeneralMotors.Infrastructure.Configurations;
+
+public class CartypeTypeConfiguration : IEntityTypeConfiguration<CarType>
+{
+    public void Configure(EntityTypeBuilder<CarType> builder)
+    {
+        builder.HasData(
+            new CarType {Id=1,Type="Sedan"},
+            new CarType {Id=2,Type="Kupe"},
+            new CarType {Id=3,Type="universal"},
+            new CarType {Id=4,Type="hetchbek" },
+            new CarType {Id=5,Type="liftbek" },
+            new CarType {Id=6,Type="limuzin" },
+            new CarType {Id=7,Type="kabriolet" },
+            new CarType {Id=8,Type="pikap" },
+            new CarType {Id=9,Type="SUV" }
+            );
+    }
+}
+
