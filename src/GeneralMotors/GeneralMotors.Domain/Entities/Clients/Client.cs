623c45c918db09fceeeb67af0de0c50b66240122
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:45 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Domain/Entities/Clients/Client.cs
@@ -0,0 +1,19 @@
+﻿using GeneralMotors.Domain.Entities.Cars;
+
+namespace GeneralMotors.Domain.Entities.Clients;
+
+public class Client:BaseEntity
+{
+    public string FullName { get; set; }
+    public string Contact { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Role { get; set; }
+
+    #region relation
+    public ICollection<CarClient>? CarClients { get; set; }
+    #endregion
+}
+
