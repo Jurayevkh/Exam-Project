ca7fcc3f490297f8c012bb04891a128e73d262e5
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:52:43 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Application/UseCases/Cars/Handlers/DeleteCarCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace GeneralMotors.Application.UseCases.Cars.Handlers;
+
+public class DeleteCarCommandHandler : AsyncRequestHandler<DeleteCarCommand>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public DeleteCarCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    protected override async Task Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+    {
+        var car = _applicationDbContext.Cars.FirstOrDefault(car=>car.Id==request.Id);
+        _applicationDbContext.Cars.Remove(car);
+        await _applicationDbContext.SaveChangesAsync(cancellationToken);
+    }
+}
+
