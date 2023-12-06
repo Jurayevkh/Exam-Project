594f096feb0e75c7d8cddf4f1e285474cece047f
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:54:42 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.Domain/Entities/CarTypes/CarType.cs
@@ -0,0 +1,13 @@
+﻿using GeneralMotors.Domain.Entities.Cars;
+
+namespace GeneralMotors.Domain.Entities.CarTypes;
+
+public class CarType:BaseEntity
+{
+    public string Type { get; set; }
+
+    #region relation
+    public Car Car { get; set; }
+    #endregion
+}
+
