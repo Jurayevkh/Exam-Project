a9c9d93db4e0924bb0c6cb125f659d25d788372a
Jurayevkh <jurayevkh@gmail.com>
2023-12-06 17:51:58 +0500

    improve

---
+++ src/GeneralMotors/GeneralMotors.API/DTOs/Dillers/UpdateDillerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GeneralMotors.API.DTOs.Dillers;
+
+public class UpdateDillerDto
+{
+    public int Id { get; set; }
+    public string Region { get; set; }
+    public string Contact { get; set; }
+}
+
