af44004a7e51750fb73fc01c77ad1abb41402e87
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:05:45 +0500

    improve

---
+++ src/QRCode/QRCode.Application/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using QRCode.Domain.Entities;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Reflection;
+global using MediatR;
+global using QRCode.Application.UseCases.Users.Commands;
+global using QRCode.Application.Abstractions;
+global using QRCode.Application.UseCases.Users.Queries;
+
