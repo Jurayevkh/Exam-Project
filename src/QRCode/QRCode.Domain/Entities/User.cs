c3b6d788d512108dda918c514dbf3a069d485bee
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:21 +0500

    improve

---
+++ src/QRCode/QRCode.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace QRCode.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id:{Id},FirstName:{FirstName},LastName:{LastName},MiddleName{MiddleName},Age:{Age},Email:{Email}";
+    }
+}
+
