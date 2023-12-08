ef834d441fccf74658251bda503338cfe84fb986
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:32 +0500

    improve

---
+++ src/QRCode/QRCode.Infrastructure/Data/QRCodeDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace QRCode.Infrastructure.Data;
+
+public class QRCodeDbContext :DbContext, IApplicationDbContext
+{
+    public QRCodeDbContext(DbContextOptions<QRCodeDbContext> options):base(options)
+    {
+
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserTypeConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
