4b4cbd5a79771500858fc03f394e8fc59a0c3412
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:23 +0500

    improve

---
+++ src/QRCode/QRCode.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace QRCode.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, QRCodeDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
+
+        return services;
+    }
+}
+
