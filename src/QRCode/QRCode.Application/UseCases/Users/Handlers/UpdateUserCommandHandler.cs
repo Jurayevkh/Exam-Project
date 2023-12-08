66230a04fd358bbd7d60930854aa2f7d2723475d
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:11 +0500

    improve

---
+++ src/QRCode/QRCode.Application/UseCases/Users/Handlers/UpdateUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace QRCode.Application.UseCases.Users.Handlers;
+
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public UpdateUserCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            User user = _applicationDbContext.Users.FirstOrDefault(user=>user.Id==request.Id);
+            if (user is null)
+                return false;
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.MiddleName = request.MiddleName;
+            user.Age = request.Age;
+            user.Email = request.Email;
+
+            _applicationDbContext.Users.Update(user);
+            var result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
