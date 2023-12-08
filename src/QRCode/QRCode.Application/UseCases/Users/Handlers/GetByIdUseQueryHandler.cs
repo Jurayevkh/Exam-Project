39f8d7854dd3beecc10ef03a77c7ceb2c3cf4746
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:09 +0500

    improve

---
+++ src/QRCode/QRCode.Application/UseCases/Users/Handlers/GetByIdUseQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace QRCode.Application.UseCases.Users.Handlers;
+
+public class GetByIdUseQueryHandler : IRequestHandler<GetByIdUserQuery, User>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public GetByIdUseQueryHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<User> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(user => user.Id == request.Id);
+
+        return user;
+    }
+}
+
