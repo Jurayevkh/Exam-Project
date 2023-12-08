2849c22c00517d1b0044c7a5e35c2e4dfdb96dee
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:30 +0500

    improve

---
+++ src/QRCode/QRCode.Infrastructure/Configurations/UserTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace QRCode.Infrastructure.Configuration;
+
+public class UserTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.MiddleName)
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Age)
+            .IsRequired();
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+    }
+}
+
