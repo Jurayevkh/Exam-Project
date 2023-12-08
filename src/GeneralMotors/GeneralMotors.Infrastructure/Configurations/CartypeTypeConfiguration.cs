dfd941f0dfeca388889a5b806d39953f1367f95d
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 14:51:39 +0500

    improve

--- src/GeneralMotors/GeneralMotors.Infrastructure/Configurations/CartypeTypeConfiguration.cs
+++ src/GeneralMotors/GeneralMotors.Infrastructure/Configurations/CartypeTypeConfiguration.cs
@@ -1,8 +1,4 @@
-﻿using GeneralMotors.Domain.Entities.CarTypes;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace GeneralMotors.Infrastructure.Configurations;
+﻿namespace GeneralMotors.Infrastructure.Configurations;
 
 public class CartypeTypeConfiguration : IEntityTypeConfiguration<CarType>
 {
