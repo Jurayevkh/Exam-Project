9a509768608ec7adb7c34c8c7bef49019c74e8d7
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:05:28 +0500

    improve

---
+++ src/QRCode/QRCode.API/DTOs/CreateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace QRCode.API.DTOs;
+
+public class CreateUserDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+}
+
