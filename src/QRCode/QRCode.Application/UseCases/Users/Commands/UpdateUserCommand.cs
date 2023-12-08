73179de4c03e7fe9a404d89b5877c4764805bfbf
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:05:58 +0500

    improve

---
+++ src/QRCode/QRCode.Application/UseCases/Users/Commands/UpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace QRCode.Application.UseCases.Users.Commands;
+
+public class UpdateUserCommand:IRequest<bool>
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+}
+
