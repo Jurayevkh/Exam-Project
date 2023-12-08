be55313ad733e8ba5e2e4f841fdcbc3eff681e5d
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:06:04 +0500

    improve

---
+++ src/QRCode/QRCode.Application/UseCases/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace QRCode.Application.UseCases.Users.Handlers;
+
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public DeleteUserCommandHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = _applicationDbContext.Users.FirstOrDefault(user=>user.Id==request.Id);
+            _applicationDbContext.Users.Remove(user);
+            var result = await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
