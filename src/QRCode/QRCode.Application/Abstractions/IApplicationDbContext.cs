2e7304c0ecacb5109e7ab2ec40c60cb574e99721
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:05:51 +0500

    improve

---
+++ src/QRCode/QRCode.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace QRCode.Application.Abstractions;
+
+public interface IApplicationDbContext
+{ 
+    public DbSet<User> Users { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
+
