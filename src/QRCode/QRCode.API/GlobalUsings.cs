79ba8dbf6918a6319cadc41f4afb6b2423cc746d
Jurayevkh <jurayevkh@gmail.com>
2023-12-08 15:05:13 +0500

    improve

---
+++ src/QRCode/QRCode.API/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using QRCode.Application;
+global using QRCode.Infrastructure;
+global using Microsoft.AspNetCore.Mvc;
+global using MediatR;
+global using QRCode.Application.UseCases.Users.Queries;
+global using QRCode.API.DTOs;
+global using QRCode.Application.UseCases.Users.Commands;
+global using QRCoder;
+global using System.Drawing;
