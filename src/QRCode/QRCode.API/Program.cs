0c077aa4eb3a5a7a88fa2493a3b5eab35a402aff
Jurayevkh <jurayevkh@gmail.com>
2023-12-11 05:19:10 +0500

    setup QRCode project

--- src/QRCode/QRCode.API/Program.cs
+++ src/QRCode/QRCode.API/Program.cs
@@ -1,26 +1,27 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost";
+    options.InstanceName = "local";
+});
 
+builder.Services.AddHostedService<CachingBackgroundService>(); 
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
